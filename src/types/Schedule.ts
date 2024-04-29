b77fbaefeadcb3c6bdd76084e1f59ab37b389d26
James Martinez <jamescmartinez@gmail.com>
2024-04-29 10:33:11 -0500

    Add request to Schedule interface

--- src/types/Schedule.ts
+++ src/types/Schedule.ts
@@ -5,33 +5,31 @@ export default interface Schedule {
   name?: string;
   description?: string;
   queue: string;
-  paused?: boolean;
+  request: Request;
   cron?: string;
   rrule?: string;
   dtstart?: string;
+  paused?: boolean;
 }
 
 export interface CreateScheduleParams {
   name?: string;
   description?: string;
   queue?: string;
-  paused?: boolean;
   request: Request;
-
   /**
    * You must pass either `cron` or `rrule` when creating a new Schedule.
    */
   cron?: string;
-
   /**
    * You must pass either `cron` or `rrule` when creating a new Schedule.
    */
   rrule?: string;
-
   /**
    * Optionally specified with `rrule`. Ignored when using `cron`.
    */
   dtstart?: string;
+  paused?: boolean;
 }
 
 export type UpdateScheduleParams = Partial<CreateScheduleParams>;
